175f7defce55fa02aaaa234e03c0368e2c7a2d47
unknown <ayat10080@Qgmail.com>
2022-12-01 12:50:10 +0500

    firstAssignment Done

---
+++ Assignmnet 1.cpp
@@ -0,0 +1,26 @@
+ #include<iostream>
+using namespace std;
+int main()
+{
+	cout << "Phir Mehr Ali Shah Arid Agriculture University, Rawalpindi,is the only of its kind on rainfed agriculture in the world" << endl;
+	cout << "The original name given to the univeristy was 'Univeristy of Arid Agriculture Rawalpindi' " << endl;
+	cout << "It was esblished ar shamsabad, Murree Road, Rawalpindion April 27, 1994, on the occasion of the first convocation of the barani agriculture college,rawalpindi." << endl;
+	cout << "The univeristy of arid agriculture bill,1994, was passedby the provinical assembly of the punjab on the 30th of april,1994," << endl;
+	cout << "And assented to by the governor of the punjab on the 14th, day of may,1994," << endl;
+	cout << "When it became an acts of the provinical assembly (published in the gazette of the punjab on 24th of May, 1994)." << endl;
+	cout << "The barani institute information techology (BIIT) was estsblished at the univeristy, in 1998," << endl;
+	cout << "after a long and deliberate consideration of the growing need of software development " << endl;
+	cout << "and the initiative launched by goverment of pakistan, public private partnership." << endl;
+	cout << "The insitute was orginally established  as a joint venture between The PMAS arid agriclture univeristy and priviate sector" << endl;
+	cout << "The insitute has state of the art computing facilite." << endl;
+	cout << "Overall there are 8 labs,numerous severs, and various apple device and computer  for IOS and Android development." << endl;
+	cout << "There are two lab s and six teaching labs,comprising of Intel multi-Core machines." << endl;
+	cout << "All the labs are networked through fast Ethernet hub s and switches." << endl;
+	cout << "The admission to BIIT are open to all person of either whatever religion, race , creed." << endl;
+	cout << "BIIT follow a blind admission policy, where the admission is granted without assessing the ability of the student to finance his education." << endl;
+	cout << "reason for migration must be genuine and plausible." << endl;
+	cout << "producation of a certificate of a good conduct and behavior from the parant institution." << endl;
+	cout << "migration are not allowed from insitute situated in rawalpindi of lslamabad." << endl;
+
+
+}
